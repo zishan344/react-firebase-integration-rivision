368b122be3ac91fd81850f35cc81c6efd8e2d293
zishan344 <zishanahmed344@gmail.com>
2022-04-09 14:57:02 +0600

    first commit

---
+++ src/component/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <nav></nav>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/component/Product/Product.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <div>
+      <h1>This is Product section</h1>
+    </div>
+  );
+};
+
+export default Product;
